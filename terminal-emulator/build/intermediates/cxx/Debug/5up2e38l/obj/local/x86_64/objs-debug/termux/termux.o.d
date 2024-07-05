46f5c77e9caea1444e50181ca8ca2f54c1b73cf9
Noureldeen Elsayed <grandgamer856@gmail.com>
2024-07-04 17:02:57 -0700

    initial commit

--- terminal-emulator/build/intermediates/cxx/Debug/5up2e38l/obj/local/x86_64/objs-debug/termux/termux.o.d
+++
@@ -1,2 +0,0 @@
-D:\dev\Boxvidra-Android\terminal-emulator\build\intermediates\cxx\Debug\5up2e38l/obj/local/x86_64/objs-debug/termux/termux.o: \
-  D:/dev/Boxvidra-Android/terminal-emulator/src/main/jni/termux.c
